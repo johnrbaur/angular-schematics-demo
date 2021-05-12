9685d6d38f1cea5685dc617715d48709d1df3e64
John Baur <john.baur@oasisdigital.com>
2021-05-12 15:24:44 -0500

    call an external schematic

--- src/smart-view/index.ts
+++ src/smart-view/index.ts
@@ -1,12 +1,12 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { externalSchematic, Rule } from '@angular-devkit/schematics';
 
 interface InputOptions {
   name: string;
 }
 
 export function smartView(options: InputOptions): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    console.log({options});
-    return tree;
-  };
+  return externalSchematic('@schematics/angular', 'component', {
+    name: options.name,
+    style: 'scss'
+  });
 }
