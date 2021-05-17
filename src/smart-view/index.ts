c06bce9d79c5f42dbd1cfec998e5e23c6f2dbdea
John Baur <john.baur@oasisdigital.com>
2021-05-17 15:35:33 -0500

    add templates

--- src/smart-view/index.ts
+++ src/smart-view/index.ts
@@ -2,13 +2,37 @@ import {
   chain,
   externalSchematic,
   Rule,
+  apply,
+  url,
+  applyTemplates,
+  move,
+  mergeWith,
+  MergeStrategy,
 } from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
 
 interface InputOptions {
   name: string;
 }
 
 export function smartView(options: InputOptions): Rule {
+  const smartTemplateSource = apply(url('../../templates/smart'), [
+    applyTemplates({
+      classify: strings.classify,
+      dasherize: strings.dasherize,
+      name: options.name,
+    }),
+    move(`/src/app/${strings.dasherize(options.name)}`),
+  ]);
+  const viewTemplateSource = apply(url('../../templates/view'), [
+    applyTemplates({
+      classify: strings.classify,
+      dasherize: strings.dasherize,
+      name: options.name,
+    }),
+    move(`/src/app/${strings.dasherize(options.name)}-view`),
+  ]);
+
   return chain([
     externalSchematic('@schematics/angular', 'component', {
       name: options.name,
@@ -18,5 +42,7 @@ export function smartView(options: InputOptions): Rule {
       name: `${options.name}-view`,
       style: 'scss',
     }),
+    mergeWith(smartTemplateSource, MergeStrategy.Overwrite),
+    mergeWith(viewTemplateSource, MergeStrategy.Overwrite),
   ]);
 }
